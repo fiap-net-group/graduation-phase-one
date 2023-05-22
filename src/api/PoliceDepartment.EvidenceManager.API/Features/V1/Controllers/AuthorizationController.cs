bfeed0122350d86846b3994a99318b047ef9ec8d
enzotlucas <enzotlucas31@gmail.com>
2023-05-22 07:51:35 -0300

    feat: changing login response

--- src/api/PoliceDepartment.EvidenceManager.API/Features/V1/Controllers/AuthorizationController.cs
+++ src/api/PoliceDepartment.EvidenceManager.API/Features/V1/Controllers/AuthorizationController.cs
@@ -12,24 +12,24 @@ namespace PoliceDepartment.EvidenceManager.API.Features.V1.Controllers
     [Produces("application/json")]
     public sealed class AuthorizationController : ControllerBase
     {
-        private readonly ILogin<LoginViewModel, BaseResponse<AccessTokenModel>> _login;
+        private readonly ILogin<LoginViewModel, BaseResponseWithValue<AccessTokenModel>> _login;
 
-        public AuthorizationController(ILogin<LoginViewModel, BaseResponse<AccessTokenModel>> login)
+        public AuthorizationController(ILogin<LoginViewModel, BaseResponseWithValue<AccessTokenModel>> login)
         {
             _login = login;
         }
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK, StatusCode = StatusCodes.Status200OK, Type = typeof(BaseResponse<AccessTokenModel>))]
+        [ProducesResponseType(StatusCodes.Status200OK, StatusCode = StatusCodes.Status200OK, Type = typeof(BaseResponseWithValue<AccessTokenModel>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, StatusCode = StatusCodes.Status400BadRequest, Type = typeof(BaseResponse))]
         public async Task<IActionResult> Login(LoginViewModel login, CancellationToken cancellationToken)
         {
             var response = await _login.RunAsync(login, cancellationToken);
 
             if (response.Success)
-                return Ok(response.Value);
+                return Ok(response);
 
-            return BadRequest(response.ResponseDetails);
+            return BadRequest(response);
         }
     }
 }
