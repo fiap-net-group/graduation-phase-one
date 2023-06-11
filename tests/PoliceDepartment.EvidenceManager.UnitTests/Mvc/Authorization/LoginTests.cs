23c1e70dd3fb24981f438e9956d1adc6a0042440
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-06-11 18:48:02 -0300

    tests: Begin mvc unit tests

---
+++ tests/PoliceDepartment.EvidenceManager.UnitTests/Mvc/Authorization/LoginTests.cs
@@ -0,0 +1,14 @@
+﻿using PoliceDepartment.EvidenceManager.MVC.Authorization.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceDepartment.EvidenceManager.UnitTests.Mvc.Authorization
+{
+    public class LoginTests
+    {
+        private readonly IAuthorizationClient _client;
+    }
+}
