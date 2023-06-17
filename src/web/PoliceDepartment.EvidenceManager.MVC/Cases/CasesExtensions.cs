9c6989a3791e0400a6e894f0de3bca56afa58bcd
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-06-17 14:02:27 -0300

    di: Adding cases dependency injection

--- src/web/PoliceDepartment.EvidenceManager.MVC/Cases/CasesExtensions.cs
+++ src/web/PoliceDepartment.EvidenceManager.MVC/Cases/CasesExtensions.cs
@@ -1,7 +1,22 @@
-﻿namespace PoliceDepartment.EvidenceManager.MVC.Cases
+﻿using PoliceDepartment.EvidenceManager.MVC.Client;
+using PoliceDepartment.EvidenceManager.MVC.Cases.Interfaces;
+using PoliceDepartment.EvidenceManager.MVC.Cases.UseCases;
+
+namespace PoliceDepartment.EvidenceManager.MVC.Cases
 {
     public static class CasesExtensions
     {
+        public static IServiceCollection AddCasesConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpClient(ClientExtensions.CasesClientName, client =>
+            {
+                client.BaseAddress = new Uri(configuration["Api:Cases:BaseAddress"]);
+            });
 
+            services.AddScoped<ICasesClient, CasesClient>();
+            services.AddScoped<IGetCasesByOfficerId, GetCasesByOfficerId>();
+
+            return services;
+        }
     }
 }
--- src/web/PoliceDepartment.EvidenceManager.MVC/Cases/UseCases/GetCasesByOfficerId.cs
+++ src/web/PoliceDepartment.EvidenceManager.MVC/Cases/UseCases/GetCasesByOfficerId.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PoliceDepartment.EvidenceManager.MVC.Cases.Interfaces;
 
 namespace PoliceDepartment.EvidenceManager.MVC.Cases.UseCases
 {
-    public class GetCasesByOfficerId
+    public class GetCasesByOfficerId : IGetCasesByOfficerId
     {
 
     }
