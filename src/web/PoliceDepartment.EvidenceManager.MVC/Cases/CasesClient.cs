1f867cdde6c859ba7a1c94eb8d63825e98f2bbae
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-06-18 01:45:06 -0300

    feat: Creating client method

--- src/web/PoliceDepartment.EvidenceManager.MVC/Cases/CasesClient.cs
+++ src/web/PoliceDepartment.EvidenceManager.MVC/Cases/CasesClient.cs
@@ -4,13 +4,18 @@ using PoliceDepartment.EvidenceManager.SharedKernel.Logger;
 using PoliceDepartment.EvidenceManager.SharedKernel.Responses;
 using PoliceDepartment.EvidenceManager.SharedKernel.ViewModels;
 using Polly.Retry;
+using System.Text;
 using System.Text.Json;
 
 namespace PoliceDepartment.EvidenceManager.MVC.Cases
 {
     public class CasesClient : BaseClient, ICasesClient
     {
+        private readonly JsonSerializerOptions _serializeOptions;
+
         private readonly string _getCasesByOfficerIdUrl;
+        private readonly string _createCaseUrl;
+
 
         public CasesClient(AsyncRetryPolicy<HttpResponseMessage> retryPolicy,
                            JsonSerializerOptions serializeOptions,
@@ -22,7 +27,10 @@ namespace PoliceDepartment.EvidenceManager.MVC.Cases
                                                          configuration,
                                                          logger)
         {
+            _serializeOptions = serializeOptions;
+
             _getCasesByOfficerIdUrl = configuration["Api:Cases:Endpoints:GetCasesByOfficerId"];
+            _createCaseUrl = configuration["Api:Cases:Endpoints:CreateCase"];
 
             ArgumentException.ThrowIfNullOrEmpty(_getCasesByOfficerIdUrl);
         }
@@ -38,6 +46,25 @@ namespace PoliceDepartment.EvidenceManager.MVC.Cases
             catch
             {
                 return new BaseResponseWithValue<IEnumerable<CaseViewModel>>().AsError(ResponseMessage.GenericError);
+            }
+        }
+
+        public async Task<BaseResponse> CreateCase(CaseViewModel caseViewModel, string accessToken, CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, _createCaseUrl)
+            {
+                Content = new StringContent(JsonSerializer.Serialize(caseViewModel, _serializeOptions),
+                                            Encoding.UTF8,
+                                            "application/json")
+            };
+
+            try
+            {
+                return await SendAuthenticatedAsync<BaseResponse>(request, accessToken, cancellationToken);
+            }
+            catch
+            {
+                return new BaseResponseWithValue<BaseResponse>().AsError(ResponseMessage.GenericError);
             }
         }
     }
