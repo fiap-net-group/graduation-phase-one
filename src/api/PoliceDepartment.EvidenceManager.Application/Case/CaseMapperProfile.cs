4736b770f22a39d4aff93229828ed76db2d163b3
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-06-19 13:53:57 -0300

    tests: Creating get case details logic unit tests

--- src/api/PoliceDepartment.EvidenceManager.Application/Case/CaseMapperProfile.cs
+++ src/api/PoliceDepartment.EvidenceManager.Application/Case/CaseMapperProfile.cs
@@ -9,22 +9,28 @@ namespace PoliceDepartment.EvidenceManager.Application.Case
         public CaseMapperProfile()
         {
             CreateMap<CaseViewModel, CaseEntity>()
+                 .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                 .ForMember(dest => dest.Officer, opt => opt.Ignore())
+                 .ForMember(dest => dest.OfficerId, opt => opt.MapFrom(src => src.OfficerId))
+                 .ForMember(dest => dest.Evidences, opt => opt.Ignore())
+                 .ReverseMap();
+
+            CreateMap<CaseEntity, CaseViewModel>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
                 .ForMember(dest => dest.OfficerId, opt => opt.MapFrom(src => src.OfficerId))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
-                .ForMember(dest => dest.Evidences, 
-                           opt => opt.MapFrom(src => src.Evidences.Select(e => 
+                .ForMember(dest => dest.Evidences,
+                           opt => opt.MapFrom(src => src.Evidences.Select(e =>
                            new EvidenceViewModel
                            {
                                Name = e.Name,
                                Description = e.Description,
                                Id = e.Id,
-                               CaseId = e.CaseId,
-                               ImageId = e.ImageId
-                           })))
-                .ReverseMap();
+                               CaseId = e.CaseId.ToString(),
+                               ImageId = e.ImageId.ToString()
+                           })));
 
             CreateMap<CreateCaseViewModel, CaseEntity>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(_ => Guid.NewGuid()))
