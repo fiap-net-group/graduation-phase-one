03ae08850b36a807d5112a9ab3fe5ce3b6daadec
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-06-19 13:37:03 -0300

    feat: Adding case details business logic and html page, creating evidences partial view

--- src/api/PoliceDepartment.EvidenceManager.Application/Case/CaseMapperProfile.cs
+++ src/api/PoliceDepartment.EvidenceManager.Application/Case/CaseMapperProfile.cs
@@ -9,10 +9,21 @@ namespace PoliceDepartment.EvidenceManager.Application.Case
         public CaseMapperProfile()
         {
             CreateMap<CaseViewModel, CaseEntity>()
-                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
-                .ForMember(dest => dest.Officer, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
                 .ForMember(dest => dest.OfficerId, opt => opt.MapFrom(src => src.OfficerId))
-                .ForMember(dest => dest.Evidences, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+                .ForMember(dest => dest.Evidences, 
+                           opt => opt.MapFrom(src => src.Evidences.Select(e => 
+                           new EvidenceViewModel
+                           {
+                               Name = e.Name,
+                               Description = e.Description,
+                               Id = e.Id,
+                               CaseId = e.CaseId,
+                               ImageId = e.ImageId
+                           })))
                 .ReverseMap();
 
             CreateMap<CreateCaseViewModel, CaseEntity>()
--- src/web/PoliceDepartment.EvidenceManager.MVC/Cases/UseCases/GetCaseDetails.cs
+++ src/web/PoliceDepartment.EvidenceManager.MVC/Cases/UseCases/GetCaseDetails.cs
@@ -1,4 +1,6 @@
-﻿using PoliceDepartment.EvidenceManager.MVC.Cases.Interfaces;
+﻿using PoliceDepartment.EvidenceManager.MVC.Authorization.Interfaces;
+using PoliceDepartment.EvidenceManager.MVC.Cases.Interfaces;
+using PoliceDepartment.EvidenceManager.SharedKernel.Logger;
 using PoliceDepartment.EvidenceManager.SharedKernel.Responses;
 using PoliceDepartment.EvidenceManager.SharedKernel.ViewModels;
 
@@ -6,11 +8,33 @@ namespace PoliceDepartment.EvidenceManager.MVC.Cases.UseCases
 {
     public class GetCaseDetails : IGetCaseDetails
     {
-        //TODO:
-        //Implement logic
-        public Task<BaseResponseWithValue<CaseViewModel>> RunAsync(Guid caseId, CancellationToken cancellationToken)
+        private readonly ICasesClient _client;
+        private readonly ILoggerManager _logger;
+        private readonly IOfficerUser _officerUser;
+
+        public GetCaseDetails(ICasesClient client, ILoggerManager logger, IOfficerUser officerUser)
         {
-            return Task.FromResult(new BaseResponseWithValue<CaseViewModel>().AsError());
+            _client = client;
+            _logger = logger;
+            _officerUser = officerUser;
+        }
+
+        public async Task<BaseResponseWithValue<CaseViewModel>> RunAsync(Guid caseId, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("MVC - Getting case details by id", (nameof(caseId), caseId), ("officerId", _officerUser.Id));
+
+            var detailsResponse = await _client.GetDetailsAsync(caseId, _officerUser.AccessToken, cancellationToken);
+
+            if (!detailsResponse.Success)
+            {
+                _logger.LogWarning("MVC - Error getting case details by id", (nameof(caseId), caseId), ("officerId", _officerUser.Id), (nameof(detailsResponse), detailsResponse));
+
+                return detailsResponse;
+            }
+
+            _logger.LogDebug("MVC - Success getting case details by id", (nameof(caseId), caseId), ("officerId", _officerUser.Id));
+
+            return detailsResponse;
         }
     }
 }
