a41e0bd6cf4447e86c1499faaa4f06add3b69133
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-06-20 08:11:01 -0300

    feat: Adding delete case logic

--- src/web/PoliceDepartment.EvidenceManager.MVC/Cases/UseCases/DeleteCase.cs
+++ src/web/PoliceDepartment.EvidenceManager.MVC/Cases/UseCases/DeleteCase.cs
@@ -1,13 +1,39 @@
-﻿using PoliceDepartment.EvidenceManager.MVC.Cases.Interfaces;
+﻿using PoliceDepartment.EvidenceManager.MVC.Authorization.Interfaces;
+using PoliceDepartment.EvidenceManager.MVC.Cases.Interfaces;
+using PoliceDepartment.EvidenceManager.SharedKernel.Logger;
 using PoliceDepartment.EvidenceManager.SharedKernel.Responses;
 
 namespace PoliceDepartment.EvidenceManager.MVC.Cases.UseCases
 {
     public class DeleteCase : IDeleteCase
     {
-        public Task<BaseResponse> RunAsync(Guid id, CancellationToken cancellationToken)
+        private readonly ICasesClient _client;
+        private readonly ILoggerManager _logger;
+        private readonly IOfficerUser _officerUser;
+
+        public DeleteCase(ICasesClient client, ILoggerManager logger, IOfficerUser officerUser)
         {
-            throw new NotImplementedException();
+            _client = client;
+            _logger = logger;
+            _officerUser = officerUser;
+        }
+
+        public async Task<BaseResponse> RunAsync(Guid id, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("MVC - Delete case logic", ("caseId", id), ("officerId", _officerUser.Id));
+
+            var response = await _client.DeleteAsync(id, _officerUser.AccessToken, cancellationToken);
+
+            if (!response.Success)
+            {
+                _logger.LogWarning("MVC - Delete case logic - Error", ("officerId", _officerUser.Id), (nameof(response), response));
+
+                return response;
+            }
+
+            _logger.LogDebug("MVC - Delete case logic - Success", ("caseId", id), ("officerId", _officerUser.Id));
+
+            return response;
         }
     }
 }
