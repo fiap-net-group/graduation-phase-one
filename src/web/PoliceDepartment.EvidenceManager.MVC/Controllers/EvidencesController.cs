3fde9e09fe86c84564c7bfb236d5d02877d9ce94
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-06-24 13:26:13 -0300

    feat: Changin mvc create evidence logic

--- src/web/PoliceDepartment.EvidenceManager.MVC/Controllers/EvidencesController.cs
+++ src/web/PoliceDepartment.EvidenceManager.MVC/Controllers/EvidencesController.cs
@@ -14,17 +14,15 @@ namespace PoliceDepartment.EvidenceManager.MVC.Controllers
         private readonly IOfficerUser _officerUser;
         private readonly ICreateEvidence _createEvidence;
         private readonly IGetEvidenceDetails _getEvidenceDetails;
-        private readonly string _serverPath;
+
         public EvidencesController(ILoggerManager logger,
                                    IOfficerUser officerUser,
                                    ICreateEvidence createEvidence,
-                                   IGetEvidenceDetails getEvidenceDetails,
-                                   IWebHostEnvironment webHostEnvironment) : base(logger)
+                                   IGetEvidenceDetails getEvidenceDetails) : base(logger)
         {
             _officerUser = officerUser;
             _createEvidence = createEvidence;
             _getEvidenceDetails = getEvidenceDetails;
-            _serverPath = webHostEnvironment.WebRootPath;
         }
 
         [HttpGet]
@@ -45,41 +43,16 @@ namespace PoliceDepartment.EvidenceManager.MVC.Controllers
                 return View("Create", model);
             }
 
-            await UploadImageToLocal(model, cancellationToken);
-
             var createEvidenceResponse = await _createEvidence.RunAsync(model, cancellationToken);
-
-            var returnUrl = Request.Query["returnUrl"].ToString();
 
             if (createEvidenceResponse.Success)
             {
-                if (!String.IsNullOrEmpty(returnUrl))
-                {
-                    return Redirect(returnUrl.ToString());
-                }
-                else
-                {
-                    return View(nameof(Create), model);
-                }
-            };
+                RedirectToReturnUrlIfSpecfied(View(nameof(Create), model));
+            }
 
             AddErrorsToModelState(createEvidenceResponse);
 
             return View(nameof(Create), model);
-        }
-
-        private async Task UploadImageToLocal(CreateEvidencePageViewModel model, CancellationToken cancellationToken)
-        {
-            string fullPath = _serverPath + "/evidences/";
-            string fullName = model.ImageId.ToString() + Path.GetExtension(model.Image.FileName);
-
-            if (!Directory.Exists(fullPath))
-                Directory.CreateDirectory(fullPath);
-
-            using (var stream = System.IO.File.Create(fullPath + fullName))
-            {
-                await model.Image.CopyToAsync(stream, cancellationToken);
-            }
         }
 
         [HttpGet]
