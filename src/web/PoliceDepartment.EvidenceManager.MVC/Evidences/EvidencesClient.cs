3bb3a4a7d2c5f1d4ac1d006754b4f3615593a36a
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2023-06-30 13:04:09 -0300

    feat: Adding remaining client methods

--- src/web/PoliceDepartment.EvidenceManager.MVC/Evidences/EvidencesClient.cs
+++ src/web/PoliceDepartment.EvidenceManager.MVC/Evidences/EvidencesClient.cs
@@ -19,7 +19,9 @@ namespace PoliceDepartment.EvidenceManager.MVC.Evidences
 
         private readonly string _createEvidenceImageUrl;
         private readonly string _createEvidenceUrl;
+        private readonly string _editUrl;
         private readonly string _deleteEvidenceImageUrl;
+        private readonly string _deleteEvidenceUrl;
         private readonly string _getEvidenceByIdUrl;
         private readonly string _getEvidenceImageUrl;
         public EvidencesClient(AsyncRetryPolicy<HttpResponseMessage> retryPolicy,
@@ -32,6 +34,8 @@ namespace PoliceDepartment.EvidenceManager.MVC.Evidences
             _createEvidenceImageUrl = configuration["Api:Evidences:Endpoints:CreateEvidenceImage"];
             _createEvidenceUrl = configuration["Api:Evidences:Endpoints:CreateEvidence"];
             _deleteEvidenceImageUrl = configuration["Api:Evidences:Endpoints:DeleteEvidenceImage"];
+            _deleteEvidenceUrl = configuration["Api:Evidences:Endpoints:DeleteEvidence"];
+            _editUrl = configuration["Api:Evidences:Endpoints:EditEvidence"];
             _getEvidenceByIdUrl = configuration["Api:Evidences:Endpoints:GetDetails"];
             _getEvidenceImageUrl = configuration["Api:Evidences:Endpoints:GetEvidenceImage"];
 
@@ -63,6 +67,39 @@ namespace PoliceDepartment.EvidenceManager.MVC.Evidences
             catch (Exception)
             {
                 return new BaseResponseWithValue<string>().AsError(ResponseMessage.GenericError);
+            }
+        }
+
+        public async Task<BaseResponse> EditAsync(Guid id, EvidenceViewModel evidenceViewModel, string accessToken, CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Patch, _editUrl + $"/{id}")
+            {
+                Content = new StringContent(JsonSerializer.Serialize(evidenceViewModel, _serializeOptions),
+                                            Encoding.UTF8,
+                                            "application/json")
+            };
+
+            try
+            {
+                return await SendAuthenticatedAsync<BaseResponse>(request, accessToken, cancellationToken);
+            }
+            catch
+            {
+                return new BaseResponse().AsError(ResponseMessage.GenericError);
+            }
+        }
+
+        public async Task<BaseResponse> DeleteEvidenceAsync(Guid id, string accessToken, CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, _deleteEvidenceUrl + $"/{id}");
+
+            try
+            {
+                return await SendAuthenticatedAsync<BaseResponseWithValue<string>>(request, accessToken, cancellationToken);
+            }
+            catch (Exception)
+            {
+                return new BaseResponse().AsError(ResponseMessage.GenericError);
             }
         }
 
--- src/web/PoliceDepartment.EvidenceManager.MVC/Evidences/Interfaces/IEvidencesClient.cs
+++ src/web/PoliceDepartment.EvidenceManager.MVC/Evidences/Interfaces/IEvidencesClient.cs
@@ -6,6 +6,8 @@ namespace PoliceDepartment.EvidenceManager.MVC.Evidences.Interfaces
     public interface IEvidencesClient
     {
         Task<BaseResponseWithValue<string>> CreateEvidenceImageAsync(IFormFile image, string accessToken, CancellationToken cancellationToken);
+        Task<BaseResponse> EditAsync(Guid id, EvidenceViewModel evidenceViewModel, string accessToken, CancellationToken cancellationToken);
+        Task<BaseResponse> DeleteEvidenceAsync(Guid id, string accessToken, CancellationToken cancellationToken);
         Task<BaseResponseWithValue<string>> DeleteEvidenceImageAsync(string imageId, string accessToken, CancellationToken cancellationToken);
         Task<BaseResponse> CreateEvidenceAsync(CreateEvidenceViewModel evidenceViewModel, string accessToken, CancellationToken cancellationToken);
         Task<BaseResponseWithValue<EvidenceViewModel>> GetEvidenceByIdAsync(Guid id, string accessToken, CancellationToken cancellationToken);
